67189b5e5e1b7528cc4e09ee89168c27511451ba
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-06-29 15:25:51 +0800

    Added order summary page

--- public/components/ordersummary/event.js
+++ public/components/ordersummary/event.js
@@ -1,8 +1,6 @@
 import styles from './component.module.css';
 
 export default function Events() {
-    document.getElementById('app').style.overflowY = "hidden";
-
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
     const total = localStorage.getItem('cartTotal') || '0.00';
     const summaryContainer = document.getElementById('summary');
